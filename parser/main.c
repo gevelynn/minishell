0047f3a27ce37b1944262a67e9813a8a270b254e
mksmdvdff <mksmdavydoff@gmail.com>
2020-11-24 02:46:24 +0300

    setting flags for separators

--- parser/main.c
+++ parser/main.c
@@ -1,23 +1,34 @@
 #include "../libft/libft.h"
 #include "../minishell.h"
-// #include <unistd.h>
-// #include <string.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <fcntl.h>
 
-int parser(char **str, t_data **elem)
+void		check_flags(char *str, t_data **temp, int i)
 {
-	int i;
-	int start;
-	int one_quotes;
-	int two_quotes;
+	if (str[i] == '|')
+		(*temp)->pipe = 1;
+	else if (str[i] == '<')
+		(*temp)->red_from = 1;
+	else if (str[i] == '>' && str[i + 1] != '>')
+		(*temp)->red_to = 1;
+	else if (str[i] == '>' && str[i + 1] == '>')
+		(*temp)->doub_red_to = 1;
+	else
+		;
+	(*temp)->next = p_lstnew();
+	*temp = (*temp)->next;
+}
+
+int			parser(char **str, t_data **elem)
+{
+	int		i;
+	int		one_quotes;
+	int		two_quotes;
+	t_data	*temp;
 
 	i = 0;
-	start = 0;
 	one_quotes = 0;
 	two_quotes = 0;
-	while ((*str)[i])
+	temp = *elem;
+	while ((*str)[i]) //поиск разделителя
 	{
 		if ((*str)[i] == '\'')
 			one_quotes++;
@@ -26,21 +37,17 @@ int parser(char **str, t_data **elem)
 		else if (((*str)[i] == ';' || (*str)[i] == '|' || (*str)[i] == '<' || (*str)[i] == '>') &&
 				(one_quotes % 2 == 0 && two_quotes % 2 == 0))
 		{
-			printf("YPCcc\n");
-			// command_search(*str, elem, start, i);
-			start = i;
+			check_flags(*str, &temp, i); // установка флагов под разделители
+			// line_search(*str, elem, i);
+			if ((*str)[i] == '>' && (*str)[i + 1] == '>')
+				i++;
 		}
 		i++;
 	}
-	if (start == 0)
-		command_search(*str, elem, 0, 0);
-	// if ((command_search(*str) != 0))
-	// 	return (-1);
-	// printf("line: %s\n", *str);
 	return (1);
 }
 
-int		main()
+int			main()
 {	
 	int		check;
 	char 	*line;
@@ -58,52 +65,3 @@ int		main()
 	free (line);
 	return (0);
 }
-
-// int main()
-// {
-// 	int i = 0;
-// 	int fd = open("test.txt", O_RDONLY);
-// 	char *line = NULL;
-// 	t_data *content = NULL;
-// 	t_data *tmp;
-// 	content = p_lstnew();
-// 	tmp = content;
-
-// 	// i = get_next_line(fd, &line);
-// 	// content->file_name = line;
-// 	while (get_next_line(fd, &line))
-// 	{
-// 		// printf("%s\n", line);
-// 		p_lstadd_back(&content, p_lstnew());
-// 		content->file_name = line;
-// 		content->pipe = i + 1;
-// 		content->red_to = i + 2;
-// 		content->doub_red_to = i + 3;
-// 		content->red_from = i + 4;
-// 		content = content->next;
-// 		i++;
-// 		line = NULL;
-// 	}
-// 	// printf("%d\n", i);
-// 	// printf("%d\n", p_lstsize(tmp));
-// 	content = tmp;
-// 	while (tmp->next)
-// 	{
-// 		printf("%s\n", tmp->file_name);
-// 		tmp = tmp->next;
-// 	}
-// 	// printf("%s\n", tmp->file_name);
-// 	return(0);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
